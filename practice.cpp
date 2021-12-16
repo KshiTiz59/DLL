69dde7cf5a2aa5c6a2b924fafca61c21f5638996
Kshitiz Kumar <92263447+KshiTiz59@users.noreply.github.com>
2021-12-16 11:49:07 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -31,75 +31,21 @@ void Display(Node *node)
     cout<<node->data<<"->";
     node=node->next;
   }
-  cout<<"END";
-}
-
-
-int main()
-  {
-  int n;
-  Node *head;
-    head=NULL;
-  cout<<"Enter the value of N:";
-  cin>>n;
-  for(int i=n;i>=1;i--)
-  {
-    head=push(&head,i);
-  }
-  Display(head);
-}
-  
-Q2:
-
-#include<bits/stdc++.h>
-using namespace std;
-class Node
-{
-  public :
-int data;
-Node *next;
-Node *prev;
-};
-
-Node *push(Node **head_ref,int d)
-{
-  Node *new_node=new Node();
-  new_node->data=d;
-  new_node->next=*head_ref;
-  new_node->prev=NULL;
-  if(*head_ref!=NULL)
-  {
-    (*head_ref)->prev=new_node;
-  }
-  *head_ref=new_node;
-  return *head_ref;
-}
-void Display(Node *node)
-{
-  while(node!=NULL)
-  {
-    cout<<node->data<<"->";
-    node=node->next;
-  }
   cout<<"END"<<endl;
 }
-void swap(**head_ref)
+void add(Node ** head_ref)
 {
-  int count =1;
   Node *p=*head_ref;
-  Node *q=*head_ref;
-  
-  while((q->next)!=NULL)
-  {
-    q=q->next;
-  }
-  while(count!=i)
-  {
-    count++;
-    p=p->next;
-    q=q->prev;
-  }
-  Node *node1=new
+  int i=1;
+while(p->next!=NULL)
+{
+  p->data=(p->data)+i;
+  p=p->next;
+  i++;
+}
+
+p->data=(p->data)+(i);
+
 }
 
 int main()
@@ -109,15 +55,24 @@ int main()
     head=NULL;
   cout<<"Enter the value of N:";
   cin>>n;
-  for(int i=n;i>=1;i--)
+  for(int i=1;i<=n;i++)
   {
-    head=push(&head,i);
+    int data;
+    cout<<"Enter the data of "<<(n-i+1)<<" node :";
+    cin>>data;
+    head=push(&head,data);
   }
   Display(head);
-  cout<<"Enter the i-th Node to Swap:";
-  cin>>i;
-  swap(&head,i);
-}
+  cout<<"list after adding:\n";
+  add(&head);
+  Display(head);
+  }
+  
+Q2:
+
+
+
+  
 
 
   
